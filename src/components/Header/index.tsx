43b5ee8db48c18170db6153d0b1f7e095eb6ce0a
Gabriel Costa <gabrielcostalavor0@gmail.com>
2022-02-03 14:24:53 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export function Header(){
     return(
         <header className={styleClass.header}>
             <div className={styleClass.headerContainer + ` container`} >
-                <div className='LogoMenu'>
+                <div className={styleClass.LogoMenu}>
                     {/**Toda vez que eu por apenas (/) vai para a pasta public */}
                     <img src="/imagens/ig.news.svg" alt="" />
                 </div>
@@ -12,8 +12,8 @@ export function Header(){
                     <div className="homeMenu"><a href="#">Home</a></div>
                     <div className="postsMenu"><a href="#">Posts</a></div>
                 </nav>
-                <div className="sing-in-header">
-                        <button className='button-login'>
+                <div className={styleClass['sing-in-header']}>
+                        <button>
                             <img src="/imagens/Github.png" alt="" />
                             <span>Sing in with GitHub</span>
                         </button>
