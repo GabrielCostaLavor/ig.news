df5e142eb641ae1d300a94eacdf8b5fe4e7e2249
Gabriel Costa <gabrielcostalavor0@gmail.com>
2022-02-07 10:57:03 -0300

    Add função de sair e nome do usuario

--- src/components/Header/SingInButton/index.tsx
+++ src/components/Header/SingInButton/index.tsx
@@ -1,16 +1,16 @@
 import styleClass from './style.module.scss'
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
-import { signIn, useSession } from "next-auth/client"
+import { signIn, useSession, signOut } from "next-auth/client"
 
 export function SingInButton(){
     const [session] = useSession();
-    
+    console.log(session)
     return session  ? (
         <button className={styleClass.buttonSingIn}>
             <FaGithub className={styleClass.logged}/>
-            <span>Nome do usuario</span>
-            <FiX className={styleClass.iconClose} />
+            <span>{session.user.name}</span>
+            <FiX onClick={() => signOut()} className={styleClass.iconClose} />
         </button>
     ) : (
         <button className={styleClass.buttonSingIn} onClick={() => {signIn('github')}}>
