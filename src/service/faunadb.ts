60a0d016316b0f591c1981477861e582ec3c0fd2
Gabriel Costa <gabrielcostalavor0@gmail.com>
2022-02-11 10:52:06 -0300

    coment

--- src/service/faunadb.ts
+++ src/service/faunadb.ts
@@ -1,5 +1,6 @@
 import { Client } from "faunadb";
 //Conectando com o faunadb
+//A pasta service é a integração com um serviço que vou usar numa api ou no back
 export const fauna = new Client({
     secret: process.env.FAUNA_KEY,
     domain: 'db.fauna.com',
