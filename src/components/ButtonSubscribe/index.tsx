ddaf9ac6be851165bee771b22ccc1e6f70f91e40
Gabriel Costa <gabrielcostalavor0@gmail.com>
2022-02-14 18:21:20 -0300

    Add router for posts

--- src/components/ButtonSubscribe/index.tsx
+++ src/components/ButtonSubscribe/index.tsx
@@ -2,6 +2,7 @@ import styleClass from './style.module.scss'
 import {useSession, singIn} from 'next-auth/client'
 import { api } from '../../service/api';
 import { getStripeJs } from '../../service/stripe-js';
+import { useRouter } from 'next/router';
 
 interface ButtonSubProps{
     priceId: string,
@@ -9,6 +10,8 @@ interface ButtonSubProps{
 export function ButtonSubiscribe({priceId} : ButtonSubProps){
     //verificando situação do usuario
     const [ session ] = useSession();
+    //Pra eu ter acesso as rotas
+    const router = useRouter()
     //Função para realizar a inscrição
     async function subscribe(){
         //Se o usuario não extive logado ele vai para a parte de login
@@ -16,9 +19,12 @@ export function ButtonSubiscribe({priceId} : ButtonSubProps){
             singIn('github')
             return;
         }
-        //console.log(session)
+        console.log(session)
+        if(session.ActiveSubscription){
+            router.push('/posts')
+            return;
+        }
         //Chamando a api que vai criar o pagamento
-
         try {
             //Ta usandondo o methodo post para publicar na api subscribe
             const response = await api.post('/subscribe')
